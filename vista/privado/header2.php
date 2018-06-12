364af16d70476fa1766d5e767b57ce165412eac3
Laura Vicente Martín <>
2018-06-13 01:19:11 +0200

    Cambios cutres

--- vista/privado/header2.php
+++ vista/privado/header2.php
@@ -10,6 +10,26 @@
 </head>
 <body>
     <div id="Top"></div>
+
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <aside class="col-12 col-md-2 p-0 bg-dark">
+                <nav class="navbar navbar-expand navbar-dark bg-dark flex-md-column flex-row align-items-start">
+                    <div class="collapse navbar-collapse">
+                        <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
+                            <li class="nav-item">
+                                <a class="nav-link pl-0" href="#">Link</a>
+                            </li>
+                            ..
+                        </ul>
+                    </div>
+                </nav>
+            </aside>
+            <main class="col">
+            </main>
+        </div>
+    </div>
+<!--
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="MenuPrincipal">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="index.html">Coldplay</a>
@@ -43,10 +63,14 @@
         </div>
       </div>
     </nav>
+    <div></div>
+
+    -->
     
     <script src="../../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
     <script src="../../assets/vendor/js/popper.min.js"></script>
     <script src="../../assets/vendor/bootstrap/bootstrap.min.js"></script>
     <script src="../../assets/js/scripts.js"></script>
+
 </body>
 </html>
---
+++ vista/publico/blog.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../assets/vendor/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/css/styles.css">
+    <title>Coldplay | HOME</title>
+</head>
+<body>
+
+    <?php include 'header.php'; ?>
+
+    <div class="container-fluid">
+        <h1>BLOG</h1>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <!-- Bloque principal -->
+            <div class="col-md-8">
+                <div class="c-blog-post">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4 imagen">
+                            <p>IMAGEN</p>
+                        </div>
+                        <div class="col-sm-12 col-md-8">
+                            <div class="categoria">Tour</div>
+                            <div class="titulo-post">Nuevos conciertos anunciados</div>
+                            <div class="desc-post"><p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. o sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos.</p></div>
+                            <div class="datos-post">
+                                <div class="fecha">23 mayo 2012</div>
+                                <div class="autor">Artista</div>
+                                <div class="comentarios">23 comentarios</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="c-blog-post">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4 imagen">
+                            <p>IMAGEN</p>
+                        </div>
+                        <div class="col-sm-12 col-md-8">
+                            <div class="categoria">Tour</div>
+                            <div class="titulo-post">Nuevos conciertos anunciados</div>
+                            <div class="desc-post"><p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. o sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos.</p></div>
+                            <div class="datos-post">
+                                <div class="fecha">23 mayo 2012</div>
+                                <div class="autor">Artista</div>
+                                <div class="comentarios">23 comentarios</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Paginacion -->
+                <div class="paginacion">
+                    <div class="row">
+                        <div class="col-12">
+                            ul
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <!-- Sidebar del blog -->
+            <div class="col-md-4">
+                <div class="c-blog-sidebar">
+                    <div class="categorias">
+                        <div class="row">
+                            <div class="col-md-12 cat-titulo"><p>Categorías</p></div>
+                            <div class="col-md-12 cat-listado">
+                                <ul>
+                                    <li class="cat">Conciertos</li>
+                                    <li class="cat">Álbumes</li>
+                                    <li class="cat">Personal</li>
+                                    <li class="cat">Noticias</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="../../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+    <script src="../../assets/vendor/js/popper.min.js"></script>
+    <script src="../../assets/vendor/bootstrap/bootstrap.min.js"></script>
+    <script src="../../assets/js/scripts.js"></script>
+
+</body>
+</html>
